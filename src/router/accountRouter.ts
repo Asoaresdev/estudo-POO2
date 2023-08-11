5a58cfc8a72ed8887a0f39d8bc368e86d02d513c
Andrea Soares <asoaresdev@gmail.com>
2023-08-11 14:32:41 -0300

    refactor: banco de dados na raiz e implementação de rotas

---
+++ src/router/accountRouter.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { AccountController } from "../controller/AccountController";
+
+export const accountRouter = express.Router()
+
+const accountCrontoller = new AccountController
+
+accountRouter.get("/", accountCrontoller.getAccounts)
+accountRouter.post("/", accountCrontoller.postAccount)
+accountRouter.put("/:id/balance", accountCrontoller.editAccount)
+accountRouter.get("/:id/balance", accountCrontoller.getBalance)
